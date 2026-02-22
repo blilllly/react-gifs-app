c113a709fdefaf905c74330dbad09a7303696381
Billy Alvear <alvearbilly@gmail.com>
2026-02-21 19:17:36 -0500

    divide-in-components

---
+++ src/gifs/components/PreviousSearches.tsx
@@ -0,0 +1,18 @@
+import type { FC } from 'react';
+
+interface Props {
+  searches: string[];
+}
+
+export const PreviousSearches: FC<Props> = ({ searches }) => {
+  return (
+    <div className="previous-searches">
+      <h2>Búsquedas previas</h2>
+      <ul className="previous-searches-list">
+        {searches.map((search) => (
+          <li>search</li>
+        ))}
+      </ul>
+    </div>
+  );
+};
